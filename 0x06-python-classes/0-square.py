e7fc710ceba58df4a1a4cf8e488b0621306d5719
elvisthomme <elvisthomme@gmail.com>
2022-06-22 09:39:11 +0100

    Fix errors

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Empty square module"""
+
 class Square:
     """Empty square class"""
 
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Square module"""
+
 class Square:
-    """Empty square class"""
+    """Square with size property"""
 
     __init__(self, size):
         self.size = size
 
     @property
+    """The size of the square"""
     def size(self):
         return self.__size
 
