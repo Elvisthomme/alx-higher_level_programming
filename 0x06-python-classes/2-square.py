34a882d475e3b447f5d8f8217256451a386c62e4
elvisthomme <elvisthomme@gmail.com>
2022-06-22 10:40:14 +0100

    Write a square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Square with size property"""
+
+    def __init__(self, size=0):
+        """Create a new square with size"""
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
