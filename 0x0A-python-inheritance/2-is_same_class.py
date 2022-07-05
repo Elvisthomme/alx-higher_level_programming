2323fd51d653b1b06b5d15ddafda42187e7f7d30
elvisthomme <elvisthomme@gmail.com>
2022-07-05 06:43:23 +0100

    Verify if an obj is an instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""0x0A-python-inheritance module"""
+
+
+def is_same_class(obj, a_class):
+    """Verify that obj is an instance of a_class"""
+    return isinstance(obj, a_class)
