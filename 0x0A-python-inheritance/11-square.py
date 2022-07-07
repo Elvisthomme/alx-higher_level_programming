35df8967a4ec95479c18680546a0a4fa3ebe44b6
elvisthomme <elvisthomme@gmail.com>
2022-07-07 09:36:02 +0100

    Fix error

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,37 +3,7 @@
 """Geometry module"""
 
 
-class BaseGeometry():
-    """BaseGeometry class"""
-    def area(self):
-        """area of BaseGeomety"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """interger validator"""
-        if (value).__class__.__name__ != int.__name__:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class"""
-    def __init__(self, width, height):
-        """Create a new rectangle"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """area of a rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """String representation of a rectangle"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Square class"""
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -3,18 +3,7 @@
 """Geometry module"""
 
 
-class BaseGeometry():
-    """BaseGeometry class"""
-    def area(self):
-        """area of BaseGeomety"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """interger validator"""
-        if (value).__class__.__name__ != int.__name__:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
