43669de879ea2a53b353db9910148e791c78b4e3
elvisthomme <elvisthomme@gmail.com>
2022-07-07 09:30:05 +0100

    Fix error

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,7 +3,7 @@
 """Geometry module"""
 
 
-BaseGeometry = __import__("7-rectangle").BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
