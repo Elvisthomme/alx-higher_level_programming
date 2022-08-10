fffc61c48ae5b88632b050da1ae6b932175bff3f
elvisthomme <elvisthomme@gmail.com>
2022-08-10 22:39:33 +0100

    Create a table called first_table in the database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- create a table called first_table in the current database in MySQL server.
+
+-- first_table description:
+-- id INT
+-- name VARCHAR(256)
+CREATE TABLE first_table(id INT, name VARCHAR(256));
