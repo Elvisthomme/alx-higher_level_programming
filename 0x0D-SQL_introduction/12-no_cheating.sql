c4b54469198c4b89e43a2c2504ec8e0827400c88
elvisthomme <elvisthomme@gmail.com>
2022-08-11 05:15:37 +0100

    Cheating is bad

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Update the score of Bob to 10 in the table second_table
+
+-- Command: UPDATE table_name SET c1=v1, c2=v2, ... [WHERE condition]
+UPDATE second_table SET score=10 WHERE name='Bob';
