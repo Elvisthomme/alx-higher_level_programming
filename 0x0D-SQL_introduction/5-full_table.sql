d2d3a9b03dad8aab24854f48570c2674f28fab4d
elvisthomme <elvisthomme@gmail.com>
2022-08-11 04:17:59 +0100

    Update

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Print the full description of the table first_table
 
--- SHOW COLUMNS FROM table_name
-SHOW COLUMNS FROM first_table;
+-- SHOW FULL COLUMNS FROM table_name
+SHOW FULL COLUMNS FROM first_table;
