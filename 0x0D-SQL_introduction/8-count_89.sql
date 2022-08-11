a5c7cc7e9da2a7c7250529ace44c45987f92e433
elvisthomme <elvisthomme@gmail.com>
2022-08-11 04:32:19 +0100

    Display the number of records with id = 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Display the number of records with id = 89 in the table first_table
+
+-- command: SELECT COUNT(colunm) FROM table_name [WHERE condition]
+SELECT COUNT(*) FROM first_table WHERE id=89;
